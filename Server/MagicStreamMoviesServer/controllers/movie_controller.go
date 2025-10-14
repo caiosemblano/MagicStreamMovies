f10814bced2acad54a343602886093ca481a473d
Caio Semblano <semblanocaio@gmail.com>
2025-10-14 18:45:08 -0300

    Pequeno avanço

--- Server/MagicStreamMoviesServer/controllers/movie_controller.go
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -10,9 +10,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"github.com/go-playground/validator/v10"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -56,5 +59,33 @@ func GetMovie() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":"invalid input"})
+			return
+		}
+		if err := validate.Struct(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "validation failed", "details": err.Error()})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add movie"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
+
 	}
 }
---
+++ Server/MagicStreamMoviesServer/models/user_model.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"time"
+
+)
+
+type User struct {
+	ID 				bson.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID 			string			`json:"user_id" bson:"user_id"`
+	FirstName 		string		    `json:"first_name" bson:"first_name" validate:"required,min=2,max=100"`
+	LastName 		string	    	`json:"last_name" bson:"last_name" validate:"required,min=2,max=100"`
+	UserName 		string	    	`json:"username" bson:"username" validate:"required,min=2,max=100"`
+	Email 			string			`json:"email" bson:"email" validate:"email,required"`
+	Password 		string			`json:"password" bson:"password" validate:"required,min=6"`
+	Role 			string			`json:"role" bson:"role" validade:"oneof=ADMIN USER"`
+	CreatedAt 		time.Time 	    `json:"created_at" bson:"created_at"`
+	UpdatedAt 		time.Time 	    `json:"updated_at" bson:"updated_at"`
+	Token 			string			`json:"token" bson:"token"`
+	RefreshToken 	string 			`json:"refresh_token" bson:"refresh_token"`
+	FavouriteGenres []Genre 		`json:"favourite_genres" bson:"favourite_genres" validate:"required,dive"`
+}
